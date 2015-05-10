9a48e64d3a7e046ae4974786e0d787d80663460e
Junyang Huang <huangjunyang47@gmail.com>
2015-05-10 10:54:54 +0800

    lunch break. oop-ing...

--- game_engine.js
+++ game_engine.js
@@ -1,10 +1,5 @@
 'use strict'
 
-
-// command = ["put", card];
-// command = ["give_up", card]
-// command = ["pick", "color"]   
-
 function log(arg){
   console.log(arg);
 }
@@ -30,31 +25,26 @@ function Card(color, value)
 
 
 
-
-// alice_ai(desk,alice_cards,"alice_id","bob_id")
-// function greedy_
-function greedy_ai(desk, self_cards, self_id, op_id)
+function hjy_ai(self_id)
 {
-  //benefif
-  //for color in desk
-  //  for card in self_cards
-  //     if feasible and benefitical 
-  //          max benefitial
-  //        
-  // or choose one from other people
-  //no any card to put, chose the most useless card to give up
+  this.id = self_id
 
-  // for(var ii = 0; ii < self.cards)
+  function get_next_command(desk, op_id)
+  {
+    var color_frqs = {}
+    for(var ii = 0; ii< desk.player_cards[self_id].length; ii++)
+    {
+      var color = desk.colors[ii]
+      if (!color_frqs[color]){color_frqs[color]=0}
+      color_frqs[color] += 1
+    }
 
-
-  // command = ["put", card];
-  // command = ["give_up", card]
-  // command = ["pick", "color"]  
-  return command;  
 
 }
 
-function Desk()
+
+
+function Desk(player_id1, player_id2)
 {
   var desk = {}
   this.unused_cards = []
@@ -62,38 +52,52 @@ function Desk()
   for (var ii = 0; ii< this.colors.length; ii++){
     var color = this.colors[ii]
     for (var jj = 1; jj <= 10; jj++) {
-        if(jj==1)
-        {
+        if(jj==1){
           var card = new Card(color,"double")
-        }else
-        {
+        }else{
            var card = new Card(color,jj)
         }
        this.unused_cards.push(card)
     }
   }
 
-  log(this.unused_cards)
+  // log(this.unused_cards)
   this.unused_cards.shuffle();
 
-  this.alice_cards = cards.splice(0, 8)
-  this.bob_cards = cards.splice(0,8)
+  this.player_cards = {}
+  this.player_cards[player_id1] = this.unused_cards.splice(0, 8)
+  this.player_cards[player_id2] = this.unused_cards.splice(0,8)
 
-
+  this.public_spcae = {}
   for (var ii = 0; ii< this.colors.length; ii++){
     var color = this.colors[ii]
-    this[color] = { "alice_id":[],
-                    "bob_id":[],
-                    "give_up":[]} 
+    this.public_spcae[color] = { player_id1:[],  player_id2:[], "give_up":[]} 
   }
 
-}
 
-function calculate_point(same_color_cards_col)
-{ 
-   if(same_color_cards_col.length == 0){return 0;}
-    var mul = 1
-    var card_point = 0
+
+   this.calculate_player_point =function( player_id){
+    result = {}
+    total = 0
+    for (var ii = 0; ii< this.colors.length; ii++){
+      var color = this.colors[ii]
+
+      point = this.calculate_player_point(player_id, color);
+      result[color] = point
+      total += point 
+    }
+    result["total"] = total
+    return result;
+  }
+
+
+  this.calculate_single_col = function(player_id, color)
+  {
+    if (!this[color]){throw "no such color";}
+   same_color_cards_col = this.public_spcae[color][player_id];
+    if(!same_color_cards_col||same_color_cards_col.length == 0){return 0;}
+
+    var mul = 1,card_point = 0
     for(var ii = 0; ii < same_color_cards_col.length;ii++){
       var value = same_color_cards_col[ii].value
       if (value == "double"){
@@ -104,60 +108,73 @@ function calculate_point(same_color_cards_col)
     }
     var point =  (card_point-20)*mul;
     return point;
+  }
+
+// command = [player_id,"put", card];
+// command = [player_id,"give_up", card]
+// command = [player_id,"pick", "from_which_color"]
+  this.apply_command = function(cmd)
+  { 
+    if (!cmd){throw "NULL CMD"}
+    var id = cmd[0], action =  cmd[1];
+    if(action == "put"){
+      //remove card from player 
+      var card = cmd[1]
+      var index = this.player_cards[id].indexOf(card);
+      array.splice(index, 1);   
+      //add to public space player site
+      this.public_spcae[card.color][id].add(card)   
+      //give playew new card   
+      this.player_cards[id].add(this.unused_cards.pop())
+
+    }else if (action == "give_up"){
+      //remove card from player
+      var card = cmd[1] 
+      var index = this.player_cards[id].indexOf(card);
+      array.splice(index, 1);  
+      //add to public space player site
+      this.public_spcae[card.color]["give_up"].add(card) 
+      //give playew new card   
+      this.player_cards[id].add(this.unused_cards.pop())  
+
+    }else if (action == "pick"){
+      //pick the card
+      var color = cmd[2]
+      assert(this.public_spcae[color]["give_up"] > 0)
+      var new_card = this.public_spcae[color]["give_up"].pop()
+      //add to the player
+      this.player_cards[id].add(new_card)
+
+    }else
+    {
+      throw "Wrong command"
+    }
+  }
 }
 
 
 
-
-function game(alice_ai, bob_ai)
+function game(id1, id2)
 {
+  
+  var desk = new Desk(id1,  id2)
+  log(desk)
 
- 
-  //a desk has five color sitep
-  //player can put 
-  // log(desk)
-
-  while(cards.length >0)
+  while(desk.unused_cards.length >0)
   {
-    var command=alice_ai(desk,"alice_id","bob_id")
-    // apply(command, desk)
-    if (cards.length == 0){break;}
-    bob_ai(desk, "bob_id", "alice_id")
-        // apply(command, desk)
+    var command=hjy_ai(desk,id1,id2)
+    desk.apply_command(command)
+
+    if (desk.unused_cards.length == 0){break;}
+
+    command=hjy_ai(desk, id2, id1)
+    desk.apply_command(command)
+
   }
 
   // var result =calculate_win_lose()
-
 }
 
-function test_calculate_point()
-{
-  var cards = [ new Card('yellow', 'double'),
-                new Card('yellow', 2),
-                new Card('yellow', 3)]
-  var point = calculate_point(cards)
-  assert(point == -30)
+var id1= "hjy",  id2 = "alice"
+game(id1, id2)
 
-   var cards =  [ new Card('yellow', 2),
-                  new Card('yellow', 3),
-                  new Card('yellow', 5),
-                  new Card('yellow', 10)]
-  var point = calculate_point(cards)
-  assert(point == 0)
-
-  var cards =  [  new Card('red', 'double'), 
-                  new Card('red', 2),
-                  new Card('red', 3),
-                  new Card('red', 5),
-                  new Card('red', 6),
-                  new Card('red', 10)]
-  var point = calculate_point(cards)
-  assert(point == 12)
-}
-
-
-
-var card = new Card("red", 1)
-log(card)
-
-// game()
